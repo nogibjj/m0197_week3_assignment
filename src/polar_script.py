aa8e7c6a0374da268cdeaa2b8165c0017f3f5eda
Mark Okello <markokello55@gmail.com>
2023-09-16 07:28:12 +0000

    "Upadte scripts and dataset"

--- src/polar_script.py
+++ src/polar_script.py
@@ -4,6 +4,40 @@ Polar function to display descriptive statistics for any file and specified or
 
 """
 
-def display_polar_desc():
-    print("Test Infra")
-    pass
+
+import polars as pl
+
+
+def descriptive_statistics(csv_file_path, selected_columns=None):
+    try:
+        df = pl.read_csv(csv_file_path)
+
+        if selected_columns is None:
+            numeric_columns = df.select(pl.col(pl.NUMERIC_DTYPES))
+        else:
+            numeric_columns = df[selected_columns]
+        for col in numeric_columns.columns:
+            series = numeric_columns[col]
+
+            mean = series.mean()
+            median = series.median()
+            std = series.std()
+
+
+    except FileNotFoundError:
+        print("The file was not found.")
+    return mean,median,std
+
+#
+# if __name__ == "__main__":
+#     csv_file_path = "datasets/cereal.csv"
+#     selected_columns = ["calories"]
+
+#     mean_str, median_str, std_str = descriptive_statistics(csv_file_path, selected_columns)
+#     mean_str = str(mean_str)
+#     median_str = str(median_str)
+#     std_str = str(std_str)
+
+#     print(f"Mean: {mean_str}, Median: {median_str}, SD: {std_str}")
+
+# prints Mean: 105.54054054054055, Median: 110.0, SD: 18.442200853112762
--- test_main.py
+++ test_main.py
@@ -1,10 +1,31 @@
 """
-Test goes here
+Testing the descriptive stats function
 
 """
 
-from src.polar_script import display_polar_desc
+from src.polar_script import descriptive_statistics
+from io import StringIO
+import sys
+
+def test_stats():
+    sys.stdout = StringIO()
+
+    csv_file_path = "/datasets/cereal.csv"
+    selected_columns = ["calories"]
+    mean_str, median_str, std_str = descriptive_statistics(csv_file_path, selected_columns)
+    mean_str = str(mean_str)
+    median_str = str(median_str)
+    std_str = str(std_str)
+    print(f"Mean: {mean_str}, Median: {median_str}, SD: {std_str}")
+
+    printed_message = sys.stdout.getvalue().strip()
+
+    sys.stdout = sys.__stdout__
+
+    expected_output = 'Mean: 105.54054054054055, Median: 110.0, SD: 18.442200853112762'
+
+    assert printed_message == expected_output
 
 
-def test_add():
-    assert  display_polar_desc() == "Test Infra"
+if __name__ == "__main__":
+    test_stats()
